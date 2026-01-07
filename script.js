24efa639d3e7f84448dca0980c5ef67696e838c2
ajkumar874134-create <ajkumar874134@gmail.com>
2026-01-08 00:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Edge case: empty subStr
+  if (subStr === "") return 0;
+
+  return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
